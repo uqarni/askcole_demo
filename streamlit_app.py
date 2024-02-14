b89144c6fc0ece5a27f516dec98615b404f6051b
uqarni <108438834+uqarni@users.noreply.github.com>
2024-02-14 08:44:08 +0000

    blank until start

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,33 +8,35 @@ st.title("AskCole: Objection Handling")
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Hey there! How can I help you level up your sales game today?"}]
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+if st.session_state.get("started", False) == True:
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# Accept user input
-if prompt := st.chat_input("How can I help?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    # Accept user input
+    if prompt := st.chat_input("How can I help?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ]
-        stream = full_response(messages)
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    st.rerun()
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ]
+            stream = full_response(messages)
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.rerun()
 
 if st.sidebar.button("Start/Restart"):
     #clear the entire session state
     st.session_state.clear()
+    st.session_state['started'] = True
     #rerun
     st.rerun()
 
