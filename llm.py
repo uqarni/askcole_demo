8b8f2dce109adbe7dcb35fabdb130fbb1f32414c
uqarni <108438834+uqarni@users.noreply.github.com>
2024-03-18 20:26:16 +0000

    get source separately

--- llm.py
+++ llm.py
@@ -148,9 +148,12 @@ def full_response(messages):
     classifier_URL = ""
     for i, chunk in enumerate(k_similar):
         retrieved_chunks += f'Chunk {i}:\n' + chunk['content'] + "\n\n"
-        ic(chunk)
-        # if classifier_URL == "":
-        #     classifier_URL = name_to_url[chunk['source']]
+        chunk_id = chunk['id']
+        #get the source column value of the chunk using the supabase api
+        source = sb.table("vdb").select("*").eq("id", chunk_id).execute()
+        source = source.data[0]['source']
+        if classifier_URL == "":
+            classifier_URL = name_to_url[chunk['source']]
 
     #save in session state
     st.session_state.chunks = retrieved_chunks
